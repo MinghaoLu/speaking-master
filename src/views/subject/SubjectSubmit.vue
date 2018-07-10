defd3eee388252b072f8dbce18b6f87c7c989bba
MinghaoLu <568432983@qq.com>
2018-07-10 11:28:59 +0800

    modify some classes' names

--- src/views/subject/SubjectSubmit.vue
+++ src/views/subject/SubjectSubmit.vue
@@ -1,23 +1,22 @@
 <template>
   <div class="app-wrapper">
-    <div class="main-left subject">
-        <p class="subject_name"> {{ $route.params.subjectId }}. {{ subjectData.name }}</p>
-        <div class= "subject_content">
-            <p>{{ subjectData.content }}
-            </p>
-        </div>
+    <div class="main-left">
+      <p class="subject-name"> {{ $route.params.subjectId }}. {{ subjectData.name }}</p>
+      <div class= "subject-content">
+        <p>{{ subjectData.content }}</p>
+      </div>
     </div>
 
-    <div class="main-right record">
-        <div class="record_controller">
-            录音controller
-        </div>
+    <div class="main-right">
+      <div class="record-controller">
+          录音controller
+      </div>
 
-        <div class="record_text_component">
-            录音文本组件区域
-        </div>
+      <div class="record-text-component">
+        录音文本组件区域
+      </div>
 
-        <el-button class="record_btn_publish" type="info" @click="publish">发布</el-button>
+      <el-button class="record-btn-publish" type="info" @click="publish">发布</el-button>
     </div>
 
   </div>
@@ -60,43 +59,44 @@ export default {
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
-    .main-left {
-        float: left;
-        width: 47%;
+    .app-wrapper {
+      margin-top: 5px;
+      display: -webkit-flex;
+      display: -moz-flex;
+      display: -ms-flex;
+      display: -o-flex;
+      display: flex;
+      justify-content: space-around;
     }
 
-    .subject {
-        .subject_content {
-            background: #EBEBEB;
-            border: 2px solid #AAA;
-            margin-top:30px;
-        }
+    .main-left {
+      width: 47%;
+      .subject-content {
+        background: #EBEBEB;
+        border: 2px solid #AAA;
+        margin-top: 30px;
+      }
     }
 
     .main-right {
-      float: right;
       width: 47%;
-      margin: 2%;
-    }
-
-    .record {
       text-align: center;
-        .record_controller {
-          height: 80px;
-          overflow: auto;
-          background: #EBEBEB;
-          border: 2px solid #AAA;
-        }
-        .record_text_component {
-          height: 240px;
-          overflow: auto;
-          margin-top: 30px;
-          background: #EBEBEB;
-          border: 2px solid #AAA;
-        }
-        .record_btn_publish {
-          margin-top: 10px;
-        }
+      .record-controller {
+        height: 80px;
+        overflow: auto;
+        background: #EBEBEB;
+        border: 2px solid #AAA;
+      }
+      .record-text-component {
+        height: 240px;
+        overflow: auto;
+        margin-top: 30px;
+        background: #EBEBEB;
+        border: 2px solid #AAA;
+      }
+      .record-btn-publish {
+        margin-top: 10px;
+      }
     }
 
 </style>
